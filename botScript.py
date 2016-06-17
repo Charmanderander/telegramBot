08fd695a0f54364a6429dd3baed6c0d670564533
jinhao <jinhao>
2016-06-17 19:52:48 +0800

    added screenshot function

--- botScript.py
+++ botScript.py
@@ -3,18 +3,21 @@ import time
 import pprint
 import telepot
 import subprocess
+import os
+
+curDir = os.path.dirname(os.path.realpath(__file__)) + '/'
 
 def handle(msg):
     pprint.pprint(msg)
+    msg = msg['text'].lower()
     # Do your stuff here ...
     if msg == 'screenshot':
 	print "taking ss"
-	subprocess.call(["scrot"," ss.png"])
-	f = open('ss.png',rb)
-	print "sending photo"
-	response = bot.sendPhoto(158951306,f)
-	f.close()
-
+	subprocess.call(["scrot", "ss.png"])
+	with open(curDir + 'ss.png','r') as f:
+		print "sending photo"
+		response = bot.sendPhoto(158951306,f)
+	
 # Getting the token from command-line is better than embedding it in code,
 # because tokens are supposed to be kept secret.
 TOKEN = sys.argv[1]
